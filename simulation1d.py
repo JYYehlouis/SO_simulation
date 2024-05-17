Fix Errors of Calculation of Intensity
Here changes the way to plot, use 4 functions to represent each
plot. Also, the calculation of E and I are also modified.

Problem:
    - the intensity are all less than 0.4 (why?)

simulation1d.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 
 from typing import Tuple
 
+# constants
+# c = 3e8
+# epsilon = 8.85e-12
+# mu = 4 * np.pi * 1e-7
+eta = 377
+
 def get_x(space: int, Fs: int) -> npt.ArrayLike:
     """
         Parameters:
@@ -26,7 +32,7 @@ def get_mask(x: npt.ArrayLike, width) -> npt.ArrayLike:
     """
     return np.where(np.abs(x) <= width, 1, 0)
 
-def get_plane_wave(x: npt.ArrayLike, magnitude: np.float128 = 1) -> npt.ArrayLike:
+def get_plane_wave(x: npt.ArrayLike, magnitude: np.float128 = 10) -> npt.ArrayLike:
     """
         Parameters:
             x (np.ndarray): a 1D array of x values
@@ -90,20 +96,42 @@ def plotTransformation(space: int = 1000, width: int = 100, Fs: int = 100):
 
         Returns: None
     """
+    # x, mask, plot mask depening on x 
+    x, mask = plot1dMask(space, width, Fs)
+
+    # E_in before the mask, E_out after the mask, E_out = F{E_in * mask}
+    # F{.} means Fourier Transform
+    E_in, freq, E_out = plot1dFT(x, mask)
+
+    # after aperture
+    cut = 0.0025
+    E_cut = plot1dCut(freq, E_out, cut)
+
+    # inverse fft
+    E_ifft = plot1dIFT(E_cut, x)
+
+    return x, mask, E_in, freq, E_out, E_cut, E_ifft
+
+
+def plot1dMask(
+    space: int = 1000, 
+    width: int = 100, 
+    Fs: int = 100
+) -> Tuple[npt.ArrayLike, npt.ArrayLike]:
+    """
+        Plots the transformation of the plane wave
+
+        Parameters:
+            space (int): the range of x values
+            width (int): the width of the mask in nanometers
+            Fs (int): the number of points in the sample
+
+        Returns: None
+    """
     # x, mask, E_in 
     x = get_x(space, Fs)
     mask = get_mask(x, width)
-    E_in = get_plane_wave(x)
-    # after the mask
-    E_out = np.multiply(E_in, np.fft.fft(mask))
-    freq, E_out = generate_freq_from_xy(E_out, x.shape[0], Fs)
-    cond = np.abs(freq) <= 0.1
-    f = freq[cond]
-    E = np.abs(E_out[cond])
     # set up the plot
-    y_max = np.max(E)
-    x_max = 0.1
-    # plot mask
     plt.figure(figsize=(8, 6))
     plt.title('Mask')
     plt.xlabel('x (nm)')
@@ -113,57 +141,87 @@ def plotTransformation(space: int = 1000, width: int = 100, Fs: int = 100):
     plt.plot(x, mask, '-')
     plt.savefig('./img/plot1dMask.png')
     plt.show()
+    
+    return x, mask
+
+
+def plot1dFT(
+    x: npt.ArrayLike, 
+    mask: npt.ArrayLike
+) -> Tuple[npt.ArrayLike, npt.ArrayLike, npt.ArrayLike]:
+    E_in = get_plane_wave(x)
+    E_out = np.fft.fft(np.multiply(mask, E_in))
+    freq, E_out = generate_freq_from_xy(E_out, x.shape[0], Fs)
+    cond = np.abs(freq) <= 0.1
+    f = freq[cond]
+    E = np.abs(E_out[cond])
+    # I = np.power(E, 2)
+    # set up the plot
+    y_max = np.max(E)
+    x_max = 0.1
     # plot Transformation
     plt.figure(figsize=(8, 6))
-    plt.title('Transformation of a Plane Wave after a Mask')
+    plt.title('FT of a Plane Wave after a Mask')
     plt.xlabel('Frequency (rad/s)')
-    plt.ylabel('Magnitude')
+    plt.ylabel('Intensity')
     plt.ylim(0, y_max)
     plt.xlim(-x_max, x_max)
     plt.plot(f, E, '-')
-    plt.savefig('./img/plot1dTransformation.png')
+    plt.savefig('./img/plot1dFT.png')
     plt.show()
+    return E_in, freq, E_out
 
-    # after aperture
-    cond_cut = np.abs(freq) <= 0.0025
+
+def plot1dCut(
+    freq: npt.ArrayLike, 
+    E_out: npt.ArrayLike, 
+    cut: np.float128
+) -> npt.ArrayLike:
+    cond_cut = np.abs(freq) <= cut
     E_cut = np.where(cond_cut, E_out, 0)
-    E_cut_mag = np.abs(E_cut)
+    E = np.abs(E_cut)
+    # I = np.power(E, 2)
+    # set up the plot
+    y_max = np.max(E)
+    x_max = 0.1
     # plot Transformation
     plt.figure(figsize=(8, 6))
     plt.title('After Aperture')
     plt.xlabel('Frequency (rad/s)')
-    plt.ylabel('Magnitude')
+    plt.ylabel('Intensity')
     plt.ylim(0, y_max)
     plt.xlim(-x_max, x_max)
-    plt.axline((-0.0025, 0), (-0.0025, y_max), color='red', linestyle='-')
-    plt.axline((0.0025, 0), (0.0025, y_max), color='red', linestyle='-')
-    plt.plot(freq, E_cut_mag, '-')
-    plt.savefig('./img/plot1dAperture.png')
+    plt.axline((-cut, 0), (-cut, y_max), color='red', linestyle='-')
+    plt.axline((cut, 0), (cut, y_max), color='red', linestyle='-')
+    plt.plot(freq, E, '-')
+    plt.savefig('./img/plot1dCut.png')
     plt.show()
+    return E_cut
 
-    # inverse fft
-    E_ifft = np.abs(np.fft.ifft(E_cut))
-    x_max_to_be_I = np.max(x[E_ifft >= 0.4])
-    x_min_to_be_I = np.min(x[E_ifft >= 0.4])
+
+def plot1dIFT(
+    E_cut: npt.ArrayLike,
+    x: npt.ArrayLike,
+    I_min: np.float128 = 0.4
+) -> npt.ArrayLike:
+    E_ifft = np.fft.ifft(E_cut)
+    E = np.abs(E_ifft)
+    I = np.power(E, 2) / (2 * eta)
+    # set up the plot
+    y_max = np.max(I)
+    x_max = np.max(x)
     # plot Transformation
     plt.figure(figsize=(8, 6))
     plt.title('Aerial Image after Inverse FFT')
     plt.xlabel('x (nm)')
-    plt.ylabel('Magnitude')
-    plt.xlim(-space, space)
-    plt.axline((x_min_to_be_I, 0), (x_min_to_be_I, 0.4), color='red', linestyle='-')
-    plt.axline((x_max_to_be_I, 0), (x_max_to_be_I, 0.4), color='red', linestyle='-')
-    plt.plot(x_min_to_be_I, 0.4, 'ro', label=f'({x_min_to_be_I:.2f}, 0.4)')
-    plt.plot(x_max_to_be_I, 0.4, 'ro', label=f'({x_max_to_be_I:.2f}, 0.4)')
-    plt.axline((x_min_to_be_I, 0.4), (x_max_to_be_I, 0.4), color='black', 
-               linestyle='--', label=f'CD = {np.min((x_max_to_be_I, -x_min_to_be_I)):.2f}')
-    plt.plot(x, E_ifft, '-')
-    plt.legend()
-    plt.savefig('./img/plot1dInverseFFT.png')
+    plt.ylabel('Intensity')
+    plt.ylim(0, y_max)
+    plt.xlim(-x_max, x_max)
+    plt.axline((-x_max, I_min), (x_max, I_min), color='red', linestyle='-')
+    plt.plot(x, I, '-')
+    plt.savefig('./img/plot1dIFT.png')
     plt.show()
-
-
-
+    return E_ifft
 
 
 if __name__ == '__main__':