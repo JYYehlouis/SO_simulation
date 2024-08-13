1e4777e81566a4e5f06cad078bc78833be247c7b
JYYehlouis <jiayao230405@gmail.com>
2024-08-13 13:57:45 +0800

    Modifications and simulations on sources on 1D
    Some modifications on environment and a sketch for simulations on
    1D area, but the sketch failed because it is based on coherent sources.
    The next is to modify it to be non coherent.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-conda install python
+conda install python==3.11.9
 pip install numpy matplotlib SciencePlots tqdm
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
