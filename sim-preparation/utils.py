01aab5433c1a443810ffca45bea4367a326f0fa0
JYYehlouis <jiayao230405@gmail.com>
2024-10-08 11:40:43 +0800

    sim-preparation partition 3 parts from ICC
    Before getting into optimization, the related part should be
    partitioned from the ICC, but the optimization part is left undone.

--- sim-preparation/utils.py
+++ sim-preparation/utils.py
@@ -1,7 +1,8 @@
-from typing import Tuple, Any
+from typing import Tuple, List
 import numpy as np
 import numpy.typing as npt
 import matplotlib.pyplot as plt
+import torch
 
 npf64 = np.float64
 npArrf64 = npt.NDArray[npf64]
@@ -89,22 +90,220 @@ def shift2Dinv(arr: npArrf64) -> npArrf64:
     new_arr2[:N + 1, :], new_arr2[N + 1:, :] = new_arr1[N:, :], new_arr1[:N, :]
     return new_arr2
 
-def mask_margins(
-    seq: npArrf64, sources: npArrf64, mask: npArrf64
-) -> npArrf64:
-    pass
+def midpoint_algo(samp1side: int, maxSize: int) -> List[Tuple[int, int]]:
+    """
+        A midpoint algorithm to find points on a circle
+        
+        Parameters
+        ----------
+        samp1side: int
+            the number of sampling, the center and the radius of the circle
 
-def midpoint_algorithm(
-    R: npf64, seq: npArrf64, sources: npArrf64, magnitude: npf64
-) -> npArrf64:
-    pass
+        Returns
+        ----------
+        List[Tuple[int, int]]
+            A list contains points on the path of the circle with radius R
+    """
+    x_center, y_center, R = samp1side, samp1side, samp1side
+    # valid point list
+    ret: List[Tuple[int, int]] = []
+    # initial point(s)
+    currX, currY = R, 0
+    # p-value to calculate if points are in the circle
+    pVal, nextpVal = 0, 1.25 - R
+    while currX > currY:
+        # update the pVal
+        pVal = nextpVal
+        # use pVal to see the position of the midpoint 
+        # and update nextpVal for the next midpoint
+        
+        # if the midpoint is in the circle
+        # means the value somehow determine by (currentX, currentY)
+        if pVal <= 0:
+            # under this condition we know that 
+            # the point (currX - 0.5, currY) is in the circle of radius R
+            # update the pVal to nextpVal with (currX - 0.5, currY + 1)
+            nextpVal = nextpVal + 2 * currY + 1
+        else:
+            # under this condition we know that
+            # the point (currX, currY) is outside the circle of radius R
+            # update the value to (currX - 1, currY + 1)
+            # for convenience we use a line to implement
+            break
+        #    p5 p3
+        # p7       p1
+        # p8       p2
+        #    p6 p4 
+        if currY == 0:
+            ret.append((currX + x_center, currY + y_center))    # p1
+            ret.append((currY + x_center, currX + y_center))    # p3
+            ret.append((currY + x_center, -currX + y_center))   # p4
+            ret.append((-currX + x_center, currY + y_center))   # p7
+        else:
+            ret.append((currX + x_center, currY + y_center))    # p1
+            ret.append((currX + x_center, -currY + y_center))   # p2
+            ret.append((currY + x_center, currX + y_center))    # p3
+            ret.append((currY + x_center, -currX + y_center))   # p4
+            ret.append((-currY + x_center, currX + y_center))   # p5
+            ret.append((-currY + x_center, -currX + y_center))  # p6
+            ret.append((-currX + x_center, currY + y_center))   # p7
+            ret.append((-currX + x_center, -currY + y_center))  # p8
+        # the next valid points by increasing 1 on y
+        currY += 1
+    # ----------------------------------------
+    # linear approach
+    x, y = currX, currY
+    while 1:
+        if len(ret) == maxSize:
+            break
+        x -= 1
+        y += 1
+        #    p5 p3
+        # p7       p1
+        # p8       p2
+        #    p6 p4
+        if (x + x_center, y + y_center) in ret:
+            break
+        ret.append((x + x_center, y + y_center))    # p1
+        ret.append((x + x_center, -y + y_center))   # p2
+        ret.append((y + x_center, x + y_center))    # p3
+        ret.append((y + x_center, -x + y_center))   # p4
+        ret.append((-y + x_center, x + y_center))   # p5
+        ret.append((-y + x_center, -x + y_center))  # p6
+        ret.append((-x + x_center, y + y_center))   # p7
+        ret.append((-x + x_center, -y + y_center))  # p8
+    # ----------------------------------------
+    return ret
 
-def pupil_margins(
-    seq: npArrf64, sources: npArrf64, pupil: str = 'circle'
-) -> npArrf64:
-    match pupil:
-        case 'circle', '_':
-            pass
+def xypos_to_idx(positions: List[Tuple[int, int]]) -> List[int]:
+    ret = []
+    for pos in positions:
+        ret.append(101 * pos[0] + pos[1])
+    ret.sort()
+    return ret
+
+def square_mask_inside(width: int, samp1side: int, d_s: npf64) -> Tuple[List[Tuple[int]], int]:
+    # the center of the circle
+    x_center = y_center = samp1side
+    # first check the boundary region of the mask
+    x = 0
+    while x * d_s < width:
+        x += 1
+    ret = []
+    y = 0
+    while y <= x:
+        if y == 0:
+            ret.append((x + x_center, y + y_center))    # p1
+            ret.append((y + x_center, x + y_center))    # p3
+            ret.append((y + x_center, -x + y_center))   # p4
+            ret.append((-x + x_center, y + y_center))   # p7
+        elif x == y:
+            ret.append((x + x_center, y + y_center))
+            ret.append((x + x_center, -y + y_center))
+            ret.append((-x + x_center, y + y_center))
+            ret.append((-x + x_center, -y + y_center))
+        else:
+            ret.append((x + x_center, y + y_center))    # p1
+            ret.append((x + x_center, -y + y_center))   # p2
+            ret.append((y + x_center, x + y_center))    # p3
+            ret.append((y + x_center, -x + y_center))   # p4
+            ret.append((-y + x_center, x + y_center))   # p5
+            ret.append((-y + x_center, -x + y_center))  # p6
+            ret.append((-x + x_center, y + y_center))   # p7
+            ret.append((-x + x_center, -y + y_center))  # p8
+        y += 1
+    return ret, x
+
+def inside_revise_for_opt(l: int, pos: List[int]) -> List[int]:
+    rep1, rep2 = pos[0], pos[2 * l]
+    rep3, rep4 = pos[-2 * l - 1], pos[-1]
+    pos.insert(2 * l + 1, rep1)
+    pos.insert(2 * l + 2, rep2)
+    pos.insert(-2 * l - 1, rep4)
+    pos.insert(-2 * l - 2, rep3)
+    return pos
+    
+
+def inside_pixels_to_outside(width: int, samp1side: int, d_s: npf64) -> List[Tuple[int, int]]:
+    # the center of the circle
+    x_center = y_center = samp1side
+    # first check the boundary region of the mask
+    x = 0
+    while x * d_s <= width:
+        x += 1
+    ret = []
+    y = 0
+    while y < x:
+        if y == 0:
+            ret.append((x + x_center, y + y_center))    # p1
+            ret.append((y + x_center, x + y_center))    # p3
+            ret.append((y + x_center, -x + y_center))   # p4
+            ret.append((-x + x_center, y + y_center))   # p7
+        else:
+            ret.append((x + x_center, y + y_center))    # p1
+            ret.append((x + x_center, -y + y_center))   # p2
+            ret.append((y + x_center, x + y_center))    # p3
+            ret.append((y + x_center, -x + y_center))   # p4
+            ret.append((-y + x_center, x + y_center))   # p5
+            ret.append((-y + x_center, -x + y_center))  # p6
+            ret.append((-x + x_center, y + y_center))   # p7
+            ret.append((-x + x_center, -y + y_center))  # p8
+        y += 1
+    return ret
+
+def optimization(Q: npArrf64, b: npArrf64):
+    J = np.zeros(shape=(Q.shape[1], 1))
+    g = 2 * (Q * J - b)
+    if np.linalg.norm(g) < 1e-4:
+        return J
+    d = -g
+    while 1:
+        gd = g.transpose() * d
+        dtqd = d.transpose() * Q * d
+        alpha = - gd / dtqd
+        J = J + alpha * d
+        # set all negatives to 0
+        cond = np.where(J < 0, True, False)
+        J[cond] = 0
+        # end set
+        g = 2 * (Q * J - b)
+        err = np.linalg.norm(g)
+        print(err)
+        if err < 1e-4:
+            return J
+        beta = g.transpose() * Q * d / dtqd
+        d = -g + beta * d
+
+def optimization_torch(Q: npArrf64, b: npArrf64):
+    if torch.cuda.is_available() == False:
+        return None
+    
+    J = np.zeros(shape=(Q.shape[1], 1))
+    torch_J = torch.from_numpy(J).to(torch.device('cuda'))
+    torch_Q = torch.from_numpy(Q).to(torch.device('cuda'))
+    torch_b = torch.from_numpy(b).to(torch.device('cuda'))
+    torch_g = (2 * (torch.mm(torch_Q, torch_J) - torch_b)).to(torch.device('cuda'))
+    torch_d = (-torch_g).to(torch.device('cuda'))
+    while 1:
+        torch_gd = torch.transpose(torch_g, 0, 1).to(torch.device('cuda'))
+        torch_dtqd = torch.mm(torch.mm(torch.transpose(torch_d, 0, 1), torch_Q), torch_d).to(torch.device('cuda'))
+        torch_alpha = - torch_gd / torch_dtqd
+        torch_J = torch_J + torch_alpha * torch_d
+        J = torch_J.to(torch.device('cpu')).numpy()
+        # set all negatives to 0
+        cond = np.where(J < 0, True, False)
+        J[cond] = 0
+        # end set
+        torch_J = torch.from_numpy(J).to(torch.device('cuda'))
+        torch_g = (2 * (torch.mm(torch_Q, torch_J) - torch_b)).to(torch.device('cuda'))
+        g = torch_g.to(torch.device('cpu')).numpy()
+        err = np.linalg(g)
+        print(err)
+        if err < 1e-4:
+            return J
+        torch_beta = torch.mm(torch.mm(torch.transpose(torch_g, 0, 1), torch_Q), torch_d) / torch_dtqd
+        torch_d = (-torch_g + torch_beta * torch_d).to(torch.device('cuda'))
 
 if __name__ == "__main__":
     source_seq(1, 0.005, 10)
+    print(midpoint_algo(50))
